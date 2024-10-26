6b03755929a3b4af4ccd5bf184bc38cd54998be0
Shane Synan <digitalcircuit36939@gmail.com>
2024-10-26 19:53:43 -0400

    ssh: Fix modern Unison support
    Fix modern Unison support in the "only-data-allowed-commands.sh" file.
    
    This is not currently used by this project, but keeps it in sync with
    other sources.

--- salt/files/server/remote/ssh/only-data-allowed-commands.sh
+++ salt/files/server/remote/ssh/only-data-allowed-commands.sh
@@ -9,9 +9,12 @@
 # Unison
 CMD_UNISON_1="/usr/bin/unison -server"
 CMD_UNISON_2="unison -server"
+CMD_UNISON_NEWRPC="unison -server __new-rpc-mode"
 
+# Always use the full-path version
 CMD_UNISON_ACTUAL="/usr/bin/unison"
 CMD_UNISON_ACTUAL_ARGS="-server"
+CMD_UNISON_ACTUAL_ARGS_NEWRPC="-server __new-rpc-mode"
 
 # SFTP
 CMD_SFTP_1="/usr/lib/sftp-server"
@@ -19,12 +22,19 @@ CMD_SFTP_2="/usr/lib/openssh/sftp-server"
 
 case "$SSH_ORIGINAL_COMMAND" in
 	"$CMD_UNISON_1")
-		# Always use the full-path version
 		"$CMD_UNISON_ACTUAL" "$CMD_UNISON_ACTUAL_ARGS"
 		;;
 	"$CMD_UNISON_2")
-		# Always use the full-path version
 		"$CMD_UNISON_ACTUAL" "$CMD_UNISON_ACTUAL_ARGS"
+		;;
+	"$CMD_UNISON_NEWRPC")
+		if [ "$(unison -version)" = "unison version 2.51.5 (ocaml 4.13.1)" ]; then
+			# Fall back to old Unison arguments if needed
+			# Allows Ubuntu 24.04's Unison to talk to Ubuntu 22.04's Unison
+			"$CMD_UNISON_ACTUAL" "$CMD_UNISON_ACTUAL_ARGS"
+		else
+			"$CMD_UNISON_ACTUAL" "$CMD_UNISON_ACTUAL_ARGS_NEWRPC"
+		fi
 		;;
 	"$CMD_SFTP_1")
 		"$CMD_SFTP_1"
